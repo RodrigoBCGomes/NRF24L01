5f186a0945d2062785c47788636031c477604985
RodrigoBCGomes <rodrigosgtau@gmail.com>
2023-05-03 17:19:48 -0300

    Criando o rádio com endereço

--- Slave/Slave.ino
+++ Slave/Slave.ino
@@ -2,6 +2,9 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+RF24 radio(9,10);                   //Pinos (CE,CSN)
+const byte endereco[6] = "123890"   //Endereço único por projeto
+
 void setup() {
 
 }
