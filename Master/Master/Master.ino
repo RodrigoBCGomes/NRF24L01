8606fb5c97873e98855d77b5cd905f7aba8f97c8
RodrigoBCGomes <rodrigosgtau@gmail.com>
2023-05-10 17:10:39 -0300

    Criando o rádio e as variáveis

--- Master/Master/Master.ino
+++ Master/Master/Master.ino
@@ -2,6 +2,12 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+RF24 radio (9,10);                                            //Criando o rádio
+const byte endereco[6] = "123890"                             //Mesmo endereço so Slave
+
+int pinoBotao = 2;                                            //Pino do botão do arduino
+int estadoBotao = 0;                                          //Variável para armazenar o estado
+
 void setup() {
 
 }
